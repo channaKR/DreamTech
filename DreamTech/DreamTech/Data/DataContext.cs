2802922d94df6acd3adc9f00a068116202d95c55
channaKR <channar.official@gmail.com>
2023-12-09 23:36:48 +0530

    Add DbContext

---
+++ DreamTech/DreamTech/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using DreamTech.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace DreamTech.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product>products {get; set;}
+        public DbSet<Customer> customers { get; set;}   
+        public DbSet<Sealer> sealers { get; set;}
+        public DbSet<Employee> employees { get; set;}
+        public DbSet<User> users  { get; set;}
+
+
+        
+
+
+    }
+}
