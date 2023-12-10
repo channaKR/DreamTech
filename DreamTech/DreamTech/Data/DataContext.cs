6a0c7d8331b4db49a10acee10762cd24e86cb46a
channaKR <channar.official@gmail.com>
2023-12-10 22:45:05 +0530

    Modified Data

--- DreamTech/DreamTech/Data/DataContext.cs
+++ DreamTech/DreamTech/Data/DataContext.cs
@@ -15,23 +15,7 @@ namespace DreamTech.Data
         public DbSet<User> users { get; set; }
         public DbSet<Product> products { get; set; }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Sealer>()
-                    .HasKey(se => new { se.SealerCode});
-            modelBuilder.Entity<Sealer>()
-                    .HasOne(se => se.User)
-                    .WithOne(u => u.Sealer);
-
-            modelBuilder.Entity<Sealer>()
-                .HasMany(se => se.Product)
-                .WithMany(pr => pr.Sealer);
-                
-                   
-
-
-
-        }
+     
 
 
     }
--- DreamTech/DreamTech/Program.cs
+++ DreamTech/DreamTech/Program.cs
@@ -1,7 +1,15 @@
+using DreamTech.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>
+    (
+        options => options.UseSqlServer(builder.Configuration
+        .GetConnectionString("DefaultConnection"))
+    ) ;
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
